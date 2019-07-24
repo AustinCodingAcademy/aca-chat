fe94665f75dfde8aebdf767f4d4c1239725dbbd1
Joseph William Hauger <joseph>
2019-07-23 19:44:50 -0500

    trying to make this work

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-
-
+const express = require("express");
+const bodyParser= require("body-parser");
+const thePort = 8080;
 let clientId = 0;
 
+let app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.listen(thePort, (err)=>{
+  if(err){
+    return console.log("Error", err);
+  }
+  console.log('Server is running on port',thePort);
+});
+
+//clients and messages
+//post clientes route
+app.post('./clients',(request, response)=>{
+  messages.clientId += 1;
+  response.send(clientId.toString());
+})
+//post messages route
+//get messages route
 
 const messages = [
   {
