c6113f62d609c71bc73048351588a81a3eac0ae7
LanodnTaylor-1 <landon.autumn.taylor@gmail.com>
2019-07-25 17:58:41 -0500

    anything

--- public/chat.js
+++ public/chat.js
@@ -23,7 +23,7 @@ function send() {
     })
     .then(res=>res.json())
     .then(data=>console.log(data))
-    document.getElementById("text").innerHTML = "";
+    document.getElementById("text").innerHTML.value = "";
 }
 
 setInterval(function(){ 
