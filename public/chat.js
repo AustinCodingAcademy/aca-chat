a18f6a3d9d989c6b3899cf9fa81fd5a0b23d6734
Joseph William Hauger <joseph>
2019-07-25 20:10:08 -0500

    making chat work!

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,18 @@
-window.onload(){
+// window.onload = ()=>{
     
+// }
+//Chat messages in UL so each chat would be an li
+
+function chat(){
+    console.log(chatMessage);
 }
+
+let chatDiv = document.getElementById('chatDiv');
+let chatBox = document.createElement('input');
+let sendChat = document.createElement('button');
+sendChat.addEventListener('click', chat);
+chatDiv.appendChild(chatBox);
+chatDiv.appendChild(sendChat);
+sendChat.innerHTML = 'Send Chat!';
+chatBox.value = "";
+let chatMessage = chatBox.value;
