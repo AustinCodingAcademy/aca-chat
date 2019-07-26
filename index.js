cbbc1453fe2c5a421d284d54aeef1844aafdc023
Cris Smith <cris.smith.fa@gmail.com>
2019-07-26 02:22:45 -0500

    added final changes and setInterval

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ let clientId = 0;
 const messages = [
   {
     clientId: 0,
-    text: "Welcome To Chat"
+    text: "Welcome To Chat, Have Fun!"
   }
 ];
 app.get('/messages',(req,res)=>{
   res.json(messages)
-})
+});
 app.post('/clients', (req,res)=>{
-  req.body = clientId++ 
+  clientId += 1 
   res.send(clientId.toString())
-})
+});
 app.post('/messages',(req,res)=>{
   let newMessage =req.body
   messages.push(newMessage)
   res.json(messages)
-})
+});
 
 app.listen(8080,console.log("Server is litening on port 8080"))
--- public/chat.js
+++ public/chat.js
@@ -1,43 +1,47 @@
 window.onload=()=>{
 postClient()
+getMessages()
 }
-let newClientId = 1
+let clientId = 0;
+
 
 function postClient(){
     let options ={
         method:"POST",
-        headers:{"Content-Type": "text/plain"},
-        body: JSON.stringify({clientId:newClientId})
+        headers:{"Content-Type": "application/json"}
     };
     fetch("/clients", options)
-    .then(res=>res.json())
-    .then(newClientId=>console.log(newClientId))
-}
+    .then(res=>res.text())
+    .then(newClientId=> clientId = newClientId)
+};
+
 function postMessage(e){
     e.preventDefault();
     let text = document.getElementById("inputtext").value;
     let options ={
         method:"POST",
         headers:{"Content-Type": "application/json"},
-        body: JSON.stringify({clientId:newClientId, text:text})
+        body: JSON.stringify({clientId:clientId, text:text})
     };
     fetch("/messages", options)
     .then(res=>res.json())
     .then(newUser=>console.log(newUser));
     getMessages()
 };
+
 function getMessages(){
     fetch("/messages")
     .then(res=>res.json())
     .then((messages)=>{
-        let messageList = "<h2>All Messages</h2>"
+        let messageList = "<h2>All Messages:</h2>"
         messages.map((m)=>{
             messageList += `
             <div>
-                <p>MESSAGE: ${m.text}</p>
+                <p>ClientId: ${m.clientId} MESSAGE: ${m.text}</p>
             </div>
             `
             });
     document.getElementById("main").innerHTML = messageList
     });
-}
+};
+setInterval(getMessages,1000)
