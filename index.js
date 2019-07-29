3f9bf7ce5c6b28ab47f091d9a4664913e52b09b9
daniela-idara <purpleelephant1987@gmail.com>
2019-07-28 21:31:12 -0500

    server running

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+let express = require("express");
 
+const bodyParser = require("body-parser");
+const thePort = 8080;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+//clients
 
 let clientId = 0;
 
+app.get('/clients', (req, res)=>{
+  res.json(clients)
+})
+
+ app.post('/clients', (req, res) => {
+  clientId =+ 1;
+  res.send(clientID.toString());
+})
+
+
+//messages
+
+app.get('/messages', (req, res)=>{
+  res.json(messages)
+})
+
+app.post('/messages', (req, res) => {
+  let message = req.body;
+  messages.push(message);
+  res.json(message);
+})
 
 const messages = [
   {
@@ -9,3 +40,13 @@ const messages = [
     text: "Welcome To Chat"
   }
 ];
+
+
+
+
+app.listen(thePort, (err) => {
+ if (err) {
+   return console.log("Error", err);
+ }
+ console.log("Web server is now listening for messages on port",thePort);
+});
