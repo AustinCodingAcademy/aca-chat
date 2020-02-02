3016ef3a4f383062e43e7ca6db2024d20a54f05c
bwdurst <bwdurst@gmail.com>
2020-02-01 20:19:49 -0600

    Working on mapping messages to DOM

--- index.js
+++ index.js
@@ -11,29 +11,30 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let chatLog = [];
-
 let clientId = 0;
 
 app.post('/clients', (req, res) => {
   clientId += 1;
   res.send(clientId.toString());
-  console.log(req)
+  // console.log(req)
 })
 
 app.get('/messages', (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
   res.json(messages)
 })
 
 app.post('/messages', (req, res) => {
-  chatLog.push({
-    "cliendId": clientId,
+  console.log(req.body)
+  messages.push({
+    "cliendId": req.body.clientId,
     "text": req.body.message
   })
   res.json(messages)
-  console.log('post successful')
+  // console.log(messages)
 })
+
+
 
 const messages = [
   {
@@ -41,6 +42,7 @@ const messages = [
     text: "Welcome To Chat"
   }
 ];
+
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
--- public/chat.js
+++ public/chat.js
@@ -3,35 +3,43 @@ const cors = require('cors');
 
 app.use(cors());
 
-console.log('I\'m loading')
+let chatLog = [];
 
 var clientId = 0;
 
 function onLoad() {
-  console.log("testing")
   fetch('http://localhost:8080/clients', {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ 'Status': "Client Logged In" })
   })
     .then(res => {
-      clientId = res;
-      console.log(res, clientId)
+     return res.json();
+    })
+    .then(myId => {
+      clientId = myId;
+      console.log(`My ID is ${clientId}.`)
     })
 }
 
-onLoad()
-
-
-
 function postMessage() {
-  let message = document.getElementById('sendMes').value;
+  let message = document.getElementById('messageBox').value;
   fetch('http://localhost:8080/messages', {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ "clientid": clientId, "message": message })
   })
     .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      console.log(data)
     })
 }
 
+function addMessage(id, message) {
+  const li = document.createElement('li')
+  const chatLog = document.getElementById('chatLog')
+  li.appendChild(document.createTextNode(`clientID ${id}: ${message}`))
+  chatLog.append(li)
+}
